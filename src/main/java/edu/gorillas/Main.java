2a8923d735fc4e36160652cb61e663112cc369fd
Sandra Eyo García <a23sandraeg@ATEIS.LOCAL>
2024-04-16 19:40:47 +0200

    feat: Agregar conexion a la BD en el Main

--- src/main/java/edu/gorillas/Main.java
+++ src/main/java/edu/gorillas/Main.java
@@ -8,6 +8,34 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Statement sentencia = null;
+        Connection conexion = null;
+
+
+        int op = 0;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String url = "jdbc:mariadb://localhost:3306/?user=root&password=";
+        try {
+            conexion = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println("No hay ningún driver que reconozca la URL especificada");
+        } catch (Exception e) {
+            System.out.println("Se ha producido algún otro error");
+        }
+
+        try {
+            sentencia = conexion.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Error");
+        }
+
+        CreacionBD.crearBase(sentencia);
 
 
     }
