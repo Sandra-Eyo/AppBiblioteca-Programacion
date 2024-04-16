1f747f8c9a5a174b68c14da86918afccf4d2aca9
Sandra Eyo García <a23sandraeg@ATEIS.LOCAL>
2024-04-16 17:34:23 +0200

    build(deps): añadir dependencia mariaDB.jdbc al pom.xml

--- src/main/java/edu/gorillas/Main.java
+++ src/main/java/edu/gorillas/Main.java
@@ -1,7 +1,16 @@
 package edu.gorillas;
+import java.sql.*;
+import java.util.Scanner;
+
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
+
+
 }
