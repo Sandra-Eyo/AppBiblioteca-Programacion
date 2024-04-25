1294f8c8604f326d482341fe682f5dc3610b4d64
Sandra-Eyo <seyo.smr@gmail.com>
2024-04-25 12:15:59 +0200

    feat: Añadir método nuevoLibro en el Main Añadir nuevo método nuevoLibro que permite añadir un nuevo libro a la base de datos siempre que el autor ya esté registrado

--- src/main/java/edu/gorillas/Main.java
+++ src/main/java/edu/gorillas/Main.java
@@ -58,6 +58,7 @@ public class Main {
                     nuevoAutor(sentencia);
                     break;
                 case 2:
+                    nuevoLibro(sentencia);
             }
 
         } while (op != 11);
@@ -76,7 +77,34 @@ public class Main {
         try {
             sentencia.executeUpdate("INSERT INTO Autores (DNI, Nombre, Nacionalidad) VALUES('" + dni + "', '" + nombre + "', '" + nacionalidad + "')");
         } catch (SQLException e) {
-            System.err.println("Se ha producido un error al insertar");
+            System.err.println("Se ha producido un error al insertar el nuevo autor");
+        }
+    }
+
+    private static void nuevoLibro(Statement sentencia) {
+        System.out.println("Dame el título del libro");
+        String titulo = sc.nextLine();
+        System.out.println("Dame el precio del libro");
+        float precio = sc.nextFloat();
+        System.out.println("Dame el nombre del autor");
+        String autorNombre = sc.nextLine();
+
+        try {
+            // Verificamos si el autor existe
+            ResultSet resultado = sentencia.executeQuery("SELECT * FROM Autores WHERE Nombre = '" + autorNombre + "'");
+
+            if (resultado.next()) {
+                // Si el autor existe, se inserta el libro
+                String autorDNI = resultado.getString("DNI");
+                sentencia.executeUpdate("INSERT INTO Libros (Titulo, Precio, Autor) VALUES('" + titulo + "', " + precio + ", '" + autorDNI + "')");
+                System.out.println("Libro insertado correctamente.");
+            } else {
+                // Si el autor no existe, se lanza el mensaje de error
+                System.out.println("Error: El autor '" + autorNombre + "' no existe en la base de datos.");
+            }
+        } catch (SQLException e) {
+            System.err.println("Se ha producido un error al insertar el libro.");
+            e.printStackTrace();
         }
     }
 
